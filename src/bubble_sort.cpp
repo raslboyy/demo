607b24123d2e61de3ed9a8319a61edc0fdbaa2ed
Ruslan <ruslankhakimov67@gmail.com>
2022-12-07 11:45:55 +0300

    refactor: use std::swap

--- src/bubble_sort.cpp
+++ src/bubble_sort.cpp
@@ -5,9 +5,7 @@ void BubbleSort(std::vector<int> &a) {
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n - i - 1; j++) {
       if (a[j] > a[i]) {
-        int tmp = a[i];
-        a[i] = a[j];
-        a[j] = a[i];
+        std::swap(a[i], a[j]);
       }
     }
   }
