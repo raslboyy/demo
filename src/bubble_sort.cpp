3c6eb8559934cba88774b5974d0ac929e663b9a1
Ruslan <ruslankhakimov67@gmail.com>
2022-12-07 11:39:19 +0300

    feat: add BubbleSort declaration

--- src/bubble_sort.cpp
+++ src/bubble_sort.cpp
@@ -1,1 +1,14 @@
 #include <bubble_sort.hpp>
+
+void BubbleSort(std::vector<int> &a) {
+  int n = a.size();
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      if (a[j] > a[i]) {
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = a[i];
+      }
+    }
+  }
+}
